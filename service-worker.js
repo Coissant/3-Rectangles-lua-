26fb100c515ad890f34a6cb37ce1b1991a228ced
Coissant <boreal067@gmail.com>
2024-08-11 19:29:00 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/index-debug.html',
+        '/index-nosplash.html',
+        '/3 rectangles.bin',
+        '/3 rectangles.data'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
